c690f3290162c37e8714dc2a1f7083dfcc8c9654
K-Road <game@chrodeprime.com>
2024-03-29 14:17:41 +1100

    create copystatic, template.html, content, generate page function

---
+++ src/copystatic.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+from block_markdown import markdown_to_html_node
+
+def copy_files_recursive(source_dir,target_dir):
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+
+    for filename in os.listdir(source_dir):
+        from_path = os.path.join(source_dir,filename)
+        tar_path = os.path.join(target_dir,filename)
+        print(f" * {from_path} > {tar_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path,tar_path)
+        else:
+            copy_files_recursive(from_path,tar_path)
+
+def extract_title(markdown):
+    if not markdown.startswith("# "):
+        raise ValueError("Invalid markwup: No H1")
+    lines = markdown.split("\n")
+    return lines[0][2:]
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as file:
+        from_md = file.read()
+    
+    with open(template_path,"r") as file:
+        template_md = file.read()
+
+    from_html = markdown_to_html_node(from_md).to_html()
+    title = extract_title(from_md)
+
+    new_html = template_md("{{ Title }}",title)
+    new_html = new_html("{{ Contents }}",from_html)
+
+    
+    with open(dest_path,"w") as file:
+        file.write(new_html)
+    
+    
+    
--- src/main.py
+++ src/main.py
@@ -2,49 +2,21 @@ from textnode import TextNode, text_type_text
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_image, text_to_textnodes
 from block_markdown import block_to_block_type
+import os
+import shutil
+from copystatic import copy_files_recursive, generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    # tnode = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    # print(tnode)
+    print("Deleting public directory")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
+    print("Copying static files to public directory")
+    copy_files_recursive(dir_path_static,dir_path_public)
 
-    # l1 = LeafNode("p", "This is a paragraph of text.")
-    # l2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-    # print(l1)
-    # print(l1.to_html())
-    # print(l2.to_html())
-
-    # node = ParentNode(
-    # "p",
-    # [
-    #     LeafNode("b", "Bold text"),
-    #     LeafNode(None, "Normal text"),
-    #     LeafNode("i", "italic text"),
-    #     LeafNode(None, "Normal text"),
-    # ],
-    # )
-    # print(node.to_html())
-
-    # text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
-    # print(extract_markdown_images(text))
-
-    # text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
-    # print(extract_markdown_links(text))
-    # node = TextNode(
-    #     "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-    #     text_type_text,
-    # )
-    # print(split_nodes_image([node]))
-    # text = "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
-    # print(text_to_textnodes(text))
-    
-    md = """1. This is **bolded** paragraph
-2.
-3. This is another paragraph with *italic* text and `code` here
-4. This is the same paragraph on a new line
-5. 
-6. This is a list
-7. with items"""
-    print(block_to_block_type(md))
+    generate_page("/content/index.md","template.html","public/index.html")
 
 main()
